aaaaf18ef644db1a40e4798b8ec20c8d2a2c0e62
Maruqayn Misha <mishamaruqyan@gmail.com>
2024-02-23 22:14:23 +0400

    added service package and created UserService class .

---
+++ src/main/java/com/service/UserService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+import com.Entity.User;
+import com.repo.UserRepo;
+
+public class UserService implements UserRepo {
+    @Override
+    public User save(User user) {
+        return null;
+    }
+}
