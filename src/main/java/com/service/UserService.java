501607d8f72a106dad81f323901441980ada42f7
Mihran <Maruqyan96@inbox.ru>
2024-02-23 22:16:58 +0400

    added new ua=ser .

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -6,6 +6,6 @@ import com.repo.UserRepo;
 public class UserService implements UserRepo {
     @Override
     public User save(User user) {
-        return null;
+        return  new User();
     }
 }
