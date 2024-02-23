11833345205ff61dcbea460bc0b3f539c68dc318
Maruqayn Misha <mishamaruqyan@gmail.com>
2024-02-23 20:59:31 +0400

    added Address clas

---
+++ src/main/java/com/Entity/Address.java
@@ -0,0 +1,17 @@
+package com.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Address {
+
+    private String street;
+    private String city;
+    private String country;
+}
